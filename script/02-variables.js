4d8b70eb0b5eca3bfaaf6652625ffbf4e662bfa8
pablopsn <pabloprina@hotmail.com>
2022-09-11 20:28:11 -0300

    2da parte tarea CLASE 9

---
+++ script/02-variables.js
@@ -0,0 +1,15 @@
+// VARIABLES
+// Una variable es un contenedeor de información
+
+var pais = "España";
+var continente = "Europa";
+var antiguedad = "2021";
+var pais_y_continente = pais + antiguedad + continente;
+
+var pais = "México";
+var continente = "Latinoamerica";
+var antiguedad = "2022";
+var pais_y_continente = pais + antiguedad + continente;
+
+console.log(pais, antiguedad, continente);
+alert(pais_y_continente);
